a1acaf3a3158fdbbfbe8af3855af4d38cc4c3bf4
kilianrdiaz <kilianrdiaz@gmail.com>
2024-07-07 17:09:55 +0200

    Add files via upload

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kroyo-di <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: kroyo-di <kroyo-di@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 18:31:51 by kroyo-di          #+#    #+#             */
-/*   Updated: 2024/06/26 18:57:02 by kroyo-di         ###   ########.fr       */
+/*   Updated: 2024/07/07 17:09:29 by kroyo-di         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -17,20 +18,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	slen;
 	char	*subs;
 
-	slen = 0;
-	while (s[slen])
-		slen++;
-	if ((slen - start) <= len)
-		return (0);
-	subs = (char *) malloc(len * sizeof(char));
+	slen = ft_strlen(s);
+	if (slen < start)
+		len = 0;
+	subs = (char *) malloc((len + 1) * sizeof(char));
 	if (subs == NULL)
-		return (0);
+		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i < len && s[start])
 	{
 		subs[i] = s[start];
 		i++;
 		start++;
 	}
+	subs[i] = '\0';
 	return (subs);
 }
